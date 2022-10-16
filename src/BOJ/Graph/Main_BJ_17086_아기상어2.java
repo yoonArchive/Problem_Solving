fd0478026b1ba4e3acb63142bdae8d7a18af174b
giyoon27 <giyoon27@naver.com>
2022-10-16 17:02:26 +0900

    REFACTOR

--- src/BOJ/Graph/Main_BJ_17086_아기상어2.java
+++ src/BOJ/Graph/Main_BJ_17086_아기상어2.java
@@ -1,7 +1,8 @@
 package BOJ.Graph;
 
 import java.io.*;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_BJ_17086_아기상어2 {
@@ -13,20 +14,17 @@ public class Main_BJ_17086_아기상어2 {
     public static int[][] map;
     public static int[] dr = {-1, 0, 1, 1, 1, 0, -1, -1};
     public static int[] dc = {-1, -1, -1, 0, 1, 1, 1, 0};
+    public static Queue<Node> queue;
 
-    public static class Shark implements Comparable<Shark> {
+    public static class Node {
         int r;
         int c;
         int distance;
 
-        public Shark(int r, int c, int distance) {
+        public Node(int r, int c, int distance) {
             this.r = r;
             this.c = c;
             this.distance = distance;
-        }
-
-        public int compareTo(Shark o) {
-            return this.distance - o.distance;
         }
     }
 
@@ -37,18 +35,23 @@ public class Main_BJ_17086_아기상어2 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        queue = new LinkedList<>();
+        int current = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                current = Integer.parseInt(st.nextToken());
+                map[i][j] = current;
+                if (current == EXIST) {
+                    queue.offer(new Node(i, j, 0));
+                }
             }
         }
+        getSafeDistance();
         int maxSafeDistance = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == NOT_EXIST) {
-                    maxSafeDistance = Math.max(maxSafeDistance, getSafeDistance(i, j));
-                }
+                maxSafeDistance = Math.max(maxSafeDistance, map[i][j]);
             }
         }
         bw.write(Integer.toString(maxSafeDistance));
@@ -57,30 +60,24 @@ public class Main_BJ_17086_아기상어2 {
         bw.close();
     }
 
-    private static int getSafeDistance(int sr, int sc) {
-        PriorityQueue<Shark> pq = new PriorityQueue<>();
-        boolean[][] isVisited = new boolean[N][M];
-        pq.offer(new Shark(sr, sc, 0));
-        isVisited[sr][sc] = true;
-        while (!pq.isEmpty()) {
-            Shark shark = pq.poll();
-            int r = shark.r;
-            int c = shark.c;
-            int distance = shark.distance;
-            if (map[r][c] == EXIST) {
-                return distance;
-            }
+    private static void getSafeDistance() {
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int r = node.r;
+            int c = node.c;
+            int distance = node.distance + 1;
             for (int d = 0; d < 8; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
-                if (!isIn(nr, nc) || isVisited[nr][nc]) {
+                if (!isIn(nr, nc)) {
                     continue;
                 }
-                pq.offer(new Shark(nr, nc, distance + 1));
-                isVisited[nr][nc] = true;
+                if (map[nr][nc] == NOT_EXIST || map[nr][nc] > distance) {
+                    map[nr][nc] = distance;
+                    queue.offer(new Node(nr, nc, distance));
+                }
             }
         }
-        return -1;
     }
 
     private static boolean isIn(int r, int c) {
