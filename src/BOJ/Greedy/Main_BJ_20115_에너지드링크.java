ad6583dc7292e9e85f36b73b2a9392cdd1e9b8d9
giyoon27 <giyoon27@naver.com>
2022-10-17 21:58:48 +0900

    REFACTOR

--- src/BOJ/Greedy/Main_BJ_20115_에너지드링크.java
+++ src/BOJ/Greedy/Main_BJ_20115_에너지드링크.java
@@ -1,9 +1,6 @@
 package BOJ.Greedy;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_BJ_20115_에너지드링크 {
@@ -12,17 +9,22 @@ public class Main_BJ_20115_에너지드링크 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Double> energyDrinks = new ArrayList<>();
+        int[] energyDrinks = new int[N];
+        int energyDrink;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
-            energyDrinks.add(Double.parseDouble(st.nextToken()));
+            energyDrink = Integer.parseInt(st.nextToken());
+            energyDrinks[i] = energyDrink;
+            max = Math.max(max, energyDrink);
         }
-        Collections.sort(energyDrinks);
-        while (--N > 0) {
-            double seleceted = energyDrinks.get(0) / 2;
-            energyDrinks.set(N, energyDrinks.get(N) + seleceted);
-            energyDrinks.remove(0);
+        double total = max;
+        for (int drink : energyDrinks) {
+            if (drink == max) {
+                continue;
+            }
+            total += (double) drink / 2;
         }
-        bw.write(Double.toString(energyDrinks.get(0)));
+        bw.write(Double.toString(total));
         br.close();
         bw.flush();
         bw.close();
