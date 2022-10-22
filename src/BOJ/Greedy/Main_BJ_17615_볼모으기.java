d17b25750d30b84902fe88a52d7402022f3a23fa
giyoon27 <giyoon27@naver.com>
2022-10-22 13:20:45 +0900

    REFACTOR

--- src/BOJ/Greedy/Main_BJ_17615_볼모으기.java
+++ src/BOJ/Greedy/Main_BJ_17615_볼모으기.java
@@ -22,25 +22,29 @@ public class Main_BJ_17615_볼모으기 {
             }
         }
         int lessBallCount = Math.min(redBalls, blueBalls);
-        char color = balls.charAt(0);
-        int index = 0, continuousCountFromLeft = 1, continuousCountFromRight = 1;
-        while (++index < N) {
-            if (balls.charAt(index) != color) {
-                break;
+        if (lessBallCount == 0) {
+            bw.write(Integer.toString(lessBallCount));
+        } else {
+            char color = balls.charAt(0);
+            int index = 0, continuousCountFromLeft = 1, continuousCountFromRight = 1;
+            while (++index < N) {
+                if (balls.charAt(index) != color) {
+                    break;
+                }
+                continuousCountFromLeft++;
             }
-            continuousCountFromLeft++;
-        }
-        int moveToLeft = color == RED ? redBalls - continuousCountFromLeft : blueBalls - continuousCountFromLeft;
-        color = balls.charAt(N - 1);
-        index = N - 1;
-        while (--index >= 0) {
-            if (balls.charAt(index) != color) {
-                break;
+            int moveToLeft = color == RED ? redBalls - continuousCountFromLeft : blueBalls - continuousCountFromLeft;
+            color = balls.charAt(N - 1);
+            index = N - 1;
+            while (--index >= 0) {
+                if (balls.charAt(index) != color) {
+                    break;
+                }
+                continuousCountFromRight++;
             }
-            continuousCountFromRight++;
+            int moveToRight = color == RED ? redBalls - continuousCountFromRight : blueBalls - continuousCountFromRight;
+            bw.write(Integer.toString(Math.min(Math.min(moveToLeft, moveToRight), lessBallCount)));
         }
-        int moveToRight = color == RED ? redBalls - continuousCountFromRight : blueBalls - continuousCountFromRight;
-        bw.write(Integer.toString(Math.min(Math.min(moveToLeft, moveToRight), lessBallCount)));
         br.close();
         bw.flush();
         bw.close();
