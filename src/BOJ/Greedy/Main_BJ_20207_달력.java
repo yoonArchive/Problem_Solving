31de67795cae29552510546d18528ed53a1ebe96
giyoon27 <giyoon27@naver.com>
2022-12-25 22:11:09 +0900

    ADD

---
+++ src/BOJ/Greedy/Main_BJ_20207_달력.java
@@ -0,0 +1,54 @@
+package BOJ.Greedy;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_BJ_20207_달력 {
+
+    public static final int SIZE = 366;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[][] schedules = new int[n][2];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            schedules[i][0] = Integer.parseInt(st.nextToken());
+            schedules[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(schedules, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o2[1] - o1[1];
+            }
+            return o1[0] - o2[0];
+        });
+        int counts[] = new int[SIZE];
+        for (int i = 0; i < n; i++) {
+            for (int day = schedules[i][0]; day <= schedules[i][1]; day++) {
+                counts[day]++;
+            }
+        }
+        int row, column, day = 0, area = 0;
+        while (++day < SIZE) {
+            row = 0;
+            column = 0;
+            if (counts[day] != 0) {
+                while (true) {
+                    row = Math.max(row, counts[day]);
+                    column++;
+                    if (++day >= SIZE || counts[day] == 0) {
+                        break;
+                    }
+                }
+            }
+            area += (row * column);
+        }
+        bw.write(Integer.toString(area));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
