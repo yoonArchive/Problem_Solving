4bdfe50fd07c18ce6f8798ac796f583ac62c7bf0
giyoon27 <giyoon27@naver.com>
2023-01-24 09:05:18 +0900

    [Gold IV] 11404 플로이드, Time: 364ms, Memory: 42000KB

--- src/BOJ/Floyd_Warshall/Main_BJ_11404_플로이드.java
+++ src/BOJ/Floyd_Warshall/Main_BJ_11404_플로이드.java
@@ -1,4 +1,4 @@
-package BOJ.Shortest_Path;
+package BOJ.Floyd_Warshall;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_BJ_11404_플로이드 {
+
 	static int INF = 10000001;
 
 	public static void main(String[] args) throws IOException {
@@ -15,12 +16,10 @@ public class Main_BJ_11404_플로이드 {
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
 		int cost[][] = new int[n + 1][n + 1];
-
 		for (int i = 0; i <= n; i++) {
 			Arrays.fill(cost[i], INF);
 			cost[i][i] = 0; // 자기자신에서 자신으로 가능 비용은 0
 		}
-
 		StringTokenizer st = null;
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -29,7 +28,6 @@ public class Main_BJ_11404_플로이드 {
 			int curCost = Integer.parseInt(st.nextToken());
 			cost[from][to] = Math.min(cost[from][to], curCost);
 		}
-
 		for (int mid = 1; mid <= n; mid++) {
 			for (int start = 1; start <= n; start++) {
 				if (start == mid)
@@ -42,15 +40,12 @@ public class Main_BJ_11404_플로이드 {
 				}
 			}
 		}
-
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				sb.append(cost[i][j] == INF ? 0 : cost[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
-
-		System.out.println(sb.toString());
+		System.out.println(sb);
 	}
-
 }
