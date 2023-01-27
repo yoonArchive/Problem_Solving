7c4ebf444a29140adaaf1c80b794c7de24689649
giyoon27 <giyoon27@naver.com>
2023-01-28 08:59:01 +0900

    [Silver IV] 1049 기타줄, Time: 80ms, Memory: 11668KB - 개선: 반복문 제거

--- src/BOJ/Greedy/Main_BJ_1049_기타줄.java
+++ src/BOJ/Greedy/Main_BJ_1049_기타줄.java
@@ -19,20 +19,14 @@ public class Main_BJ_1049_기타줄 {
             setCost = Math.min(setCost, Integer.parseInt(st.nextToken()));
             singleCost = Math.min(singleCost, Integer.parseInt(st.nextToken()));
         }
-        boolean isSetCheaper = setCost / 6 < singleCost ? true : false;
+        boolean isSetCheaper = setCost / PACKAGE < singleCost ? true : false;
         int cost = 0;
         if (!isSetCheaper) {
             cost = N * singleCost;
         } else {
-            while (N > 0) {
-                if (N / PACKAGE > 0) {
-                    cost += setCost;
-                    N -= PACKAGE;
-                } else {
-                    cost += Math.min(setCost, N * singleCost);
-                    break;
-                }
-            }
+            int setCount = N / PACKAGE;
+            N -= PACKAGE * setCount;
+            cost += (setCost * setCount) + Math.min(setCost, N * singleCost);
         }
         bw.write(Integer.toString(cost));
         br.close();
