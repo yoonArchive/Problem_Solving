59f8df8b6d2072563bf10ae12e9acab5c67c3066
giyoon27 <giyoon27@naver.com>
2023-02-06 11:53:29 +0900

    [Gole III] 6087 레이저 통신, Time: 144ms, Memory: 14796KB

--- src/BOJ/Shortest_Path/Main_BJ_6087_레이저통신.java
+++ src/BOJ/Shortest_Path/Main_BJ_6087_레이저통신.java
@@ -6,27 +6,29 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main_BJ_6087_레이저통신 {
-    static final char C = 'C';
-    static final char WALL = '*';
-    static int[] dr = {0, 0, 1, -1};
-    static int[] dc = {1, -1, 0, 0};
+
+    public static final char C = 'C';
+    public static final char WALL = '*';
+
+    public static int[] dr = {0, 0, 1, -1};
+    public static int[] dc = {1, -1, 0, 0};
 
     private static class Point implements Comparable<Point> {
         int r;
         int c;
-        int mirror;
+        int mirrors;
         int dir;
 
-        public Point(int r, int c, int mirror, int dir) {
+        public Point(int r, int c, int mirrors, int dir) {
             this.r = r;
             this.c = c;
-            this.mirror = mirror;
+            this.mirrors = mirrors;
             this.dir = dir;
         }
 
         @Override
         public int compareTo(Point o) {
-            return this.mirror - o.mirror;
+            return this.mirrors - o.mirrors;
         }
     }
 
@@ -37,15 +39,15 @@ public class Main_BJ_6087_레이저통신 {
         int W = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
         char[][] map = new char[H][W];
-        int[][] visit = new int[H][W];
+        int[][][] visit = new int[H][W][4];
         PriorityQueue<Point> pq = new PriorityQueue<>();
         int endR = 0;
         int endC = 0;
         boolean flag = false;
         for (int i = 0; i < H; i++) {
-            Arrays.fill(visit[i], Integer.MAX_VALUE);
             String line = br.readLine();
             for (int j = 0; j < W; j++) {
+                Arrays.fill(visit[i][j], Integer.MAX_VALUE);
                 char c = line.charAt(j);
                 map[i][j] = c;
                 if (c == C) {
@@ -55,7 +57,9 @@ public class Main_BJ_6087_레이저통신 {
                         pq.add(new Point(i, j, 0, 2));
                         pq.add(new Point(i, j, 0, 3));
                         flag = true;
-                        visit[i][j] = 0;
+                        for (int d = 0; d < 4; d++) {
+                            visit[i][j][d] = 0;
+                        }
                     } else {
                         endR = i;
                         endC = j;
@@ -68,28 +72,22 @@ public class Main_BJ_6087_레이저통신 {
             Point point = pq.poll();
             int r = point.r;
             int c = point.c;
-            int mirror = point.mirror;
+            int mirrors = point.mirrors;
             int dir = point.dir;
             if (r == endR && c == endC) {
-                answer = mirror;
+                answer = mirrors;
                 break;
             }
             for (int d = 0; d < 4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
+                int nMirrors = d == dir ? mirrors : mirrors + 1;
                 if (nr < 0 || nc < 0 || nr >= H || nc >= W || map[nr][nc] == WALL) {
                     continue;
                 }
-                if (d == dir) {
-                    if (visit[nr][nc] >= mirror) {
-                        visit[nr][nc] = mirror;
-                        pq.add(new Point(nr, nc, mirror, d));
-                    }
-                } else {
-                    if (visit[nr][nc] > mirror) {
-                        visit[nr][nc] = mirror + 1;
-                        pq.add(new Point(nr, nc, mirror + 1, d));
-                    }
+                if (visit[nr][nc][d] > nMirrors) {
+                    pq.add(new Point(nr, nc, nMirrors, d));
+                    visit[nr][nc][d] = nMirrors;
                 }
             }
         }
@@ -98,5 +96,4 @@ public class Main_BJ_6087_레이저통신 {
         bw.flush();
         bw.close();
     }
-
 }
